e505d0ebef2bd4d0a3f607fee597ff0e9578754a
bobby <bobbywang@xiaoe-tech.com>
2019-11-05 16:05:23 +0800

    Demo测试店铺参数修改

--- sample/app/src/main/java/com/xiaoeshopsdk/sample/utils/Const.java
+++ sample/app/src/main/java/com/xiaoeshopsdk/sample/utils/Const.java
@@ -5,15 +5,15 @@ package com.xiaoeshopsdk.sample.utils;
  */
 public class Const {
 	public static final String SHOP_URL = constant(
-			"https://appbq69bphk4153.sdk.xiaoe-tech.com/",
-			"http://134.175.39.17:9887/");
+			"https://app38itor341547.sdk.xiaoe-tech.com/",
+			"https://134.175.39.17:9887/");
 
 	public static final String APP_ID = constant(
-			"appbQ69BphK4153",
+			"app38itOR341547",
 			"appdRx8JicQ9960");
 
 	public static final String CLIENT_ID = constant(
-			"bm3TJyxMQJG5",
+			"KfeSEfFWTwTzfkE",
 			"KfeSEfFWTwTzfkE");
 
 	public static final String LOGIN_URL = SHOP_URL + "sdk_api/xe.account.login.test/1.0.0";
