8940738c8eb87a18340613b7fc1609a2dd38b430
bobby <bobbywang@xiaoe-tech.com>
2019-12-13 16:52:20 +0800

    opt: SDK版本升级到1.0.6

--- app/src/main/java/com/xiaoeshopsdk/sample/SdkDemoApplication.java
+++ app/src/main/java/com/xiaoeshopsdk/sample/SdkDemoApplication.java
@@ -9,6 +9,7 @@ public class SdkDemoApplication extends Application{
     @Override
     public void onCreate(){
         super.onCreate();
+        //为了使用的稳定性，建议此处指定使用X5内核的WebView
         XiaoEWeb.init(this, Const.APP_ID, Const.CLIENT_ID, XiaoEWeb.WebViewType.X5);
     }
 }
--- app/src/main/java/com/xiaoeshopsdk/sample/utils/Const.java
+++ app/src/main/java/com/xiaoeshopsdk/sample/utils/Const.java
@@ -6,13 +6,14 @@ package com.xiaoeshopsdk.sample.utils;
 public class Const {
 
 	//三方 App 用户购买小鹅店铺sdk获取的店铺Url
-	public static final String SHOP_URL = "https://app38itor341547.sdk.xiaoe-tech.com/";
+	public static final String SHOP_URL = "https://app38itOR341547.sdk.xiaoe-tech.com/";
 	//三方 App 用户注册使用小鹅店铺，取得店铺appID
 	public static final String APP_ID = "app38itOR341547";
 	//三方 App 用户购买小鹅店铺sdk接入服务，取得sdkAppId
-	public static final String CLIENT_ID = "KfeSEfFWTwTzfkE";
+	public static final String CLIENT_ID = "883pzzGyzynE72G";
 
+	//下面两个接口不建议三方 App 用户直接在自己的app端进行调用，而是三方 App 用户后台写一个sdk登录的接口提供给自己的app使用，
+	//当app请求后台的这个接口，由三方 App 用户的后台调用下面两个接口（我司的SDK登录接口），三方 App 用户的后台调用成功，把token返回给app
 	public static final String GET_TOKEN_URL = "http://api.xiaoe-tech.com/token";
 	public static final String LOGIN_URL = "http://api.xiaoe-tech.com/xe.sdk.account.login/1.0.0";
-	public static final String LOGOUT_URL = "http://api.xiaoe-tech.com/xe.sdk.account.logout/1.0.0";
 }
