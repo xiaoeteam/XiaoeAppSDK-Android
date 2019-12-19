64570962c235a5f01d7ec244b0bc8532446c575b
bobby <bobbywang@xiaoe-tech.com>
2019-12-19 10:24:08 +0800

    opt: Demo登录功能修改

--- app/src/main/java/com/xiaoeshopsdk/sample/XeSdkDemoActivity.java
+++ app/src/main/java/com/xiaoeshopsdk/sample/XeSdkDemoActivity.java
@@ -1,24 +1,44 @@
 package com.xiaoeshopsdk.sample;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import com.xiaoe.shop.webcore.XEToken;
 import com.xiaoe.shop.webcore.XiaoEWeb;
 import com.xiaoe.shop.webcore.bridge.JsBridgeListener;
 import com.xiaoe.shop.webcore.bridge.JsCallbackResponse;
 import com.xiaoe.shop.webcore.bridge.JsInteractType;
+import com.xiaoeshopsdk.sample.net.LoginEntity;
+import com.xiaoeshopsdk.sample.net.OkHttpHelper;
+import com.xiaoeshopsdk.sample.net.SimpleCallBack;
 import com.xiaoeshopsdk.sample.utils.Const;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import okhttp3.Request;
+import okhttp3.Response;
 
-public class XeSdkDemoActivity extends AppCompatActivity{
+public class XeSdkDemoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AlertDialog mLoginDlg;
+    private Button mConfirmBtn, mCancelBtn;
+    private EditText mUserNameEdit;
 
     private XiaoEWeb xiaoEWeb;
+    private OkHttpHelper mOkHttpHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +52,7 @@ public class XeSdkDemoActivity extends AppCompatActivity{
             actionBar.setDisplayShowCustomEnabled(true);
         }
 
+        mOkHttpHelper = OkHttpHelper.getInstance();
         initView();
         initEvent();
     }
@@ -43,6 +64,13 @@ public class XeSdkDemoActivity extends AppCompatActivity{
     }
 
     private void initView() {
+        View loginLayout = LayoutInflater.from(XeSdkDemoActivity.this).inflate(R.layout.login_dialog_layout, null);
+        mConfirmBtn = loginLayout.findViewById(R.id.confirm_login_btn);
+        mCancelBtn = loginLayout.findViewById(R.id.cancel_login_btn);
+        mUserNameEdit = loginLayout.findViewById(R.id.user_name_edit);
+        mLoginDlg = new AlertDialog.Builder(XeSdkDemoActivity.this).setView(loginLayout).create();
+        mLoginDlg.setCancelable(false);
+
         RelativeLayout webLayout = findViewById(R.id.web_layout);
         xiaoEWeb = XiaoEWeb.with(this)
                 .setWebParent(webLayout, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
@@ -60,21 +88,40 @@ public class XeSdkDemoActivity extends AppCompatActivity{
                     case JsInteractType.LOGIN_ACTION:
                         //H5登录态请求 这里三方 APP 应该调用登陆接口，获取到token_key, token_value
                         //如果登录成功，通过XiaoEWeb.sync(XEToken)方法同步登录态到H5页面
-                        //如果登录失败，通过XiaoEWeb.syncNot()清除登录态
-                        Toast.makeText(XeSdkDemoActivity.this, "SDK未获取登录态，请先登录！", Toast.LENGTH_SHORT).show();
+                        //如果登录失败，通过XiaoEWeb.syncNot()清除登录态（注意用户需要实现自己的SDK的登录）
+                        if (mLoginDlg != null && !mLoginDlg.isShowing())
+                            mLoginDlg.show();
                         break;
 
                     case JsInteractType.SHARE_ACTION:
                         //H5分享请求回调，通过 response.getResponseData() 获取分享的数据，这里三方 APP 自行分享登录方法
                         Toast.makeText(XeSdkDemoActivity.this, response.getResponseData(), Toast.LENGTH_SHORT).show();
                         break;
-
-                    case JsInteractType.LOGOUT_ACTION:
-                        xiaoEWeb.syncNot();
-                        break;
                 }
             }
         });
+        mConfirmBtn.setOnClickListener(this);
+        mCancelBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.confirm_login_btn:
+                if (!TextUtils.isEmpty(mUserNameEdit.getText())) {
+                    doLogin(mUserNameEdit.getText().toString());
+                } else {
+                    Toast.makeText(XeSdkDemoActivity.this, R.string.user_name_empty_tip, Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.cancel_login_btn:
+                if (mLoginDlg != null && mLoginDlg.isShowing()) {
+                    mLoginDlg.dismiss();
+                    xiaoEWeb.loginCancel();
+                }
+                break;
+        }
     }
 
     @Override
@@ -88,32 +135,21 @@ public class XeSdkDemoActivity extends AppCompatActivity{
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case android.R.id.home:
-                this.finish();
+                finish();
                 return true;
             case R.id.share:
-                //触发分享
                 xiaoEWeb.share();
                 return true;
             case R.id.logout:
-                //清除登录态
                 xiaoEWeb.syncNot();
                 return true;
             case R.id.fresh:
-                //刷新页面
                 if (xiaoEWeb != null)
                     xiaoEWeb.getUrlLoader().reload();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        //WebView的返回处理
-        if (xiaoEWeb.handlerKeyEvent(keyCode, event))
-            return true;
-        return super.onKeyDown(keyCode, event);
     }
 
     @Override
@@ -126,5 +162,46 @@ public class XeSdkDemoActivity extends AppCompatActivity{
     protected void onDestroy() {
         super.onDestroy();
         xiaoEWeb.webLifeCycle().onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (xiaoEWeb.handlerKeyEvent(keyCode, event))
+            return true;
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void doLogin(String userName){
+        Map<String, Object> params = new HashMap<>(4);
+        params.put("user_id", userName);
+        params.put("app_user_id", userName);
+        params.put("secret_key", Const.SECRET_KEY);
+        params.put("sdk_app_id", Const.CLIENT_ID);
+        params.put("app_id", Const.APP_ID);
+        mOkHttpHelper.post(Const.SDK_LOGIN_URL, params, new SimpleCallBack<LoginEntity>(XeSdkDemoActivity.this) {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                mLoginDlg.dismiss();
+                Toast.makeText(XeSdkDemoActivity.this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(Response response, LoginEntity loginEntity) {
+                mLoginDlg.dismiss();
+                if (loginEntity != null && loginEntity.getCode() == 0 && loginEntity.getData() != null) {
+                    Toast.makeText(XeSdkDemoActivity.this, R.string.login_success, Toast.LENGTH_SHORT).show();
+                    XEToken token = new XEToken(loginEntity.getData().getToken_key(), loginEntity.getData().getToken_value());
+                    xiaoEWeb.sync(token);
+                } else {
+                    Toast.makeText(XeSdkDemoActivity.this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onError(Response response, int code, Exception e) {
+                mLoginDlg.dismiss();
+                Toast.makeText(XeSdkDemoActivity.this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/xiaoeshopsdk/sample/utils/Const.java
+++ app/src/main/java/com/xiaoeshopsdk/sample/utils/Const.java
@@ -5,12 +5,16 @@ package com.xiaoeshopsdk.sample.utils;
  */
 public class Const {
 
-	//三方 App 用户购买小鹅店铺sdk获取的店铺Url
+	//Demo测试店铺的登录接口（三方 App 用户需要实现SDK的登录，具体请看文档）
+	public static final String SDK_LOGIN_URL = "https://app38itOR341547.sdk.xiaoe-tech.com/sdk_api/xe.account.login.test/1.0.0";
+	//Demo测试店铺的Url（三方 App 用户购买小鹅店铺sdk获取店铺Url  替换）
 	public static final String SHOP_URL = "https://app38itOR341547.sdk.xiaoe-tech.com/";
-	//三方 App 用户注册使用小鹅店铺，取得店铺appID
+	//Demo测试店铺的appID（三方 App 用户注册使用小鹅店铺，取得店铺appID  替换）
 	public static final String APP_ID = "app38itOR341547";
-	//三方 App 用户购买小鹅店铺sdk接入服务，取得sdkAppId
+	//Demo测试店铺的sdkAppId（三方 App 用户购买小鹅店铺sdk获取sdkAppId 替换）
 	public static final String CLIENT_ID = "883pzzGyzynE72G";
+	//Demo测试店铺的秘钥（三方 App 用户购买小鹅店铺sdk获取的秘钥 替换）
+	public static final String SECRET_KEY = "dfomGwT7JRWWnzY3okZ6yTkHtgNPTyhr";
 
 	//下面两个接口不建议三方 App 用户直接在自己的app端进行调用，而是三方 App 用户后台写一个sdk登录的接口提供给自己的app使用，
 	//当app请求后台的这个接口，由三方 App 用户的后台调用下面两个接口（我司的SDK登录接口），三方 App 用户的后台调用成功，把token返回给app
